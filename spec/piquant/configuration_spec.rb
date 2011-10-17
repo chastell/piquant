3e8b1bf12a75abc21d2a0dfd294c1d274d13a8d5
Piotr Szotkowski <chastell@chastell.net>
2011-10-17 23:54:27 +0200

    reset Configuration to a sane default (foul smell here)

--- spec/piquant/configuration_spec.rb
+++ spec/piquant/configuration_spec.rb
@@ -7,8 +7,12 @@ module Piquant describe Configuration do
   describe '.api_root, .api_root=' do
     it 'allows reading and setting the API root (defaulting to CCLab’s)' do
       Configuration.api_root.must_equal 'http://cecyf.megivps.pl/api/json/'
-      Configuration.api_root = 'http://some.other/url'
-      Configuration.api_root.must_equal 'http://some.other/url'
+      begin
+        Configuration.api_root = 'http://some.other/url'
+        Configuration.api_root.must_equal 'http://some.other/url'
+      ensure
+        Configuration.api_root = 'http://cecyf.megivps.pl/api/json/'
+      end
     end
   end
 
